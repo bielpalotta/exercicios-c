93f85c31a15c5b306de08f5e02d14fafa518afff
Gabriel Lucas Palotta <bielpalotta75@gmail.com>
2026-08-23 13:22:12 -0300

    Add exercicio5.C with basic constants and main function

---
+++ exercicio5.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MAX_ALUNOS 40
+#define MAX_PRODUTOS 500
+#define PI 3.1415926535
+#define ANO_ATUAL 2026
+
+int main() {
+    printf("Numero maximo de alunos: %d\n", MAX_ALUNOS);
+    printf("Quantidade maxima de produtos em estoque: %d\n", MAX_PRODUTOS);
+    printf("Valor de PI: %.10f\n", PI);
+    printf("Ano atual: %d\n", ANO_ATUAL);
+
+    return 0;
+}
